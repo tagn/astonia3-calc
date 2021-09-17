bdb4f2492354adf20c7e648ab73ac2ff5b9ab3e8
Austin Quenneville <aquennev@gmail.com>
2021-09-17 16:06:45 -0400

    [WIP] clean up stat tests

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -6,40 +6,40 @@ from src.stats import *
 def stat():
     return Stat("Sword", [Attr.INT, Attr.AGI, Attr.STR], Cost.SKILL, Group.PRIMARY, 1)
 
+# helpers
+
+def verify_stat_values(stat_instance, cost, value):
+    assert stat_instance.cost == cost
+    assert stat_instance.value == value
+
+# tests
+
 def test_set_base(stat):
     stat.set_base_cost(PlayerClass.WARRIOR)
-    assert stat.cost == 21
-    assert stat.value == 1
-    assert stat.total_xp_used == 0
+    verify_stat_values(stat, 21, 1)
 
 def test_increase(stat):
     stat.set_base_cost(PlayerClass.WARRIOR)
     stat.increase(PlayerClass.WARRIOR)
-    assert stat.cost == 34
-    assert stat.value == 2
+    verify_stat_values(stat, 34, 2)
 
 def test_decrease(stat):
     stat.set_base_cost(PlayerClass.WARRIOR)
     stat.increase(PlayerClass.WARRIOR)
     stat.decrease(PlayerClass.WARRIOR)
-    assert stat.cost == 21
-    assert stat.value == 1
+    verify_stat_values(stat, 21, 1)
 
 def test_multiple_increase_decrease(stat):
     stat.set_base_cost(PlayerClass.WARRIOR)
     for _ in range(5):
         stat.increase(PlayerClass.WARRIOR)
-    assert stat.cost == 133
-    assert stat.value == 6
+    verify_stat_values(stat, 133, 6)
     for _ in range(5):
         stat.increase(PlayerClass.WARRIOR)
-    assert stat.cost == 409
-    assert stat.value == 11
+    verify_stat_values(stat, 409, 11)
     for _ in range(5):
         stat.decrease(PlayerClass.WARRIOR)
-    assert stat.cost == 133
-    assert stat.value == 6
+    verify_stat_values(stat, 133, 6)
     for _ in range(5):
         stat.decrease(PlayerClass.WARRIOR)
-    assert stat.cost == 21
-    assert stat.value == 1
+    verify_stat_values(stat, 21, 1)
